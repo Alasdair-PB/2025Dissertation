762b7cba76e2e79b041d004747f2fe9beffe44f9
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2025-07-04 17:27:20 +0100

    Deformation & texture start

---
+++ Dissertation/Plugins/VoxelRendering/Source/ComputeDispatchers/Private/TextureGenerator.cpp
@@ -0,0 +1,136 @@
+#include "TextureGenerator.h"
+#include "ComputeDispatchers.h"
+#include "CommonRenderResources.h"
+#include "RenderGraph.h"
+#include "ComputeDispatchers/Public/PlanetGeneratorDispatcher.h"
+#include "PixelShaderUtils.h"
+#include "Runtime/RenderCore/Public/RenderGraphUtils.h"
+#include "MeshPassProcessor.inl"
+#include "StaticMeshResources.h"
+#include "DynamicMeshBuilder.h"
+#include "RenderGraphResources.h"
+#include "GlobalShader.h"
+#include "UnifiedBuffer.h"
+#include "CanvasTypes.h"
+#include "MaterialShader.h"
+
+DECLARE_STATS_GROUP(TEXT("TextureGenerator"), STATGROUP_PlanetGenerator, STATCAT_Advanced);
+DECLARE_CYCLE_STAT(TEXT("TextureGenerator Execute"), STAT_PlanetGenerator_Execute, STATGROUP_PlanetGenerator);
+
+class FTextureGenerator : public FGlobalShader
+{
+	DECLARE_GLOBAL_SHADER(FTextureGenerator);
+	SHADER_USE_PARAMETER_STRUCT(FTextureGenerator, FGlobalShader);
+
+	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER(FVector3f, leafPosition)
+		SHADER_PARAMETER(uint32, leafDepth)
+		SHADER_PARAMETER(uint32, nodeIndex)
+		SHADER_PARAMETER_UAV(Buffer<float>, inIsoValues)
+		SHADER_PARAMETER_UAV(RWBuffer<float>, outIsoValues)
+		SHADER_PARAMETER(uint32, voxelsPerAxis)
+		SHADER_PARAMETER(float, baseDepthScale)
+		SHADER_PARAMETER(float, isoLevel)
+	END_SHADER_PARAMETER_STRUCT()
+
+	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters) {
+		return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
+	}
+
+	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
+	{
+		FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
+		const FPermutationDomain PermutationVector(Parameters.PermutationId);
+		OutEnvironment.SetDefine(TEXT("THREADS_X"), NUM_THREADS_TextureGenerator_X);
+		OutEnvironment.SetDefine(TEXT("THREADS_Y"), NUM_THREADS_TextureGenerator_Y);
+		OutEnvironment.SetDefine(TEXT("THREADS_Z"), NUM_THREADS_TextureGenerator_Z);
+	}
+};
+
+IMPLEMENT_GLOBAL_SHADER(FPlanetBiomeGenerator, "/ComputeDispatchersShaders/TextureGenerator.usf", "TextureGenerator", SF_Compute);
+
+void AddSphereGeneratorPass(FRDGBuilder& GraphBuilder, FPlanetGeneratorDispatchParams& Params, FRDGBufferUAVRef OutIsoUAV) {
+	FTextureGenerator::FParameters* PassParams = GraphBuilder.AllocParameters<FTextureGenerator::FParameters>();
+	PassParams->size = Params.Input.size;
+	PassParams->seed = Params.Input.seed;
+	PassParams->baseDepthScale = Params.Input.baseDepthScale;
+	PassParams->planetScaleRatio = Params.Input.planetScaleRatio;
+	PassParams->outIsoValues = OutIsoUAV;
+
+	const auto ShaderMap = GetGlobalShaderMap(GMaxRHIFeatureLevel);
+	const TShaderMapRef<FTextureGenerator> ComputeShader(ShaderMap);
+	auto GroupCount = FComputeShaderUtils::GetGroupCount(FIntVector(PassParams->size, PassParams->size, PassParams->size), FComputeShaderUtils::kGolden2DGroupSize);
+
+	GraphBuilder.AddPass(RDG_EVENT_NAME("Texture Generator"), PassParams, ERDGPassFlags::AsyncCompute,
+		[PassParams, ComputeShader, GroupCount](FRHIComputeCommandList& RHICmdList) {
+			FComputeShaderUtils::Dispatch(RHICmdList, ComputeShader, *PassParams, GroupCount); }
+	);
+
+}
+
+void FPlanetGeneratorInterface::DispatchRenderThread(FRHICommandListImmediate& RHICmdList, FPlanetGeneratorDispatchParams Params, TFunction<void(FPlanetGeneratorOutput OutputVal)> AsyncCallback) {
+
+	FRDGBuilder GraphBuilder(RHICmdList);
+	{
+		SCOPE_CYCLE_COUNTER(STAT_PlanetGenerator_Execute);
+		DECLARE_GPU_STAT(PlanetGenerator)
+		RDG_EVENT_SCOPE(GraphBuilder, "PlanetGenerator");
+		RDG_GPU_STAT_SCOPE(GraphBuilder, PlanetGenerator);
+
+		typename FTextureGenerator::FPermutationDomain NoisePermutationVector;
+		TShaderMapRef<FTextureGenerator> NoiseComputeShader(GetGlobalShaderMap(GMaxRHIFeatureLevel), NoisePermutationVector);
+		bool bIsTextureShaderValid = NoiseComputeShader.IsValid();
+
+		if (bIsTextureShaderValid) {
+			const int isoValueCount = Params.Input.size * Params.Input.size * Params.Input.size;
+
+			TArray<float> OutIsoValues;
+			TArray<float> OutTypeValues;
+
+			OutIsoValues.Init(-1, isoValueCount);
+			OutTypeValues.Init(-1, isoValueCount);
+
+			FRDGBufferRef OutIsoValuesBuffer = CreateStructuredBuffer(GraphBuilder, TEXT("IsoValues_SB"), sizeof(float), isoValueCount, OutIsoValues.GetData(), isoValueCount * sizeof(float));
+			FRDGBufferRef OutTypeValuesBuffer = CreateStructuredBuffer(GraphBuilder, TEXT("IsoValues_SB"), sizeof(int), isoValueCount, OutIsoValues.GetData(), isoValueCount * sizeof(int));
+
+			FRDGBufferUAVRef OutIsoValuesUAV = GraphBuilder.CreateUAV(OutIsoValuesBuffer);
+			FRDGBufferUAVRef OutTypeValuesUAV = GraphBuilder.CreateUAV(OutTypeValuesBuffer);
+			FRDGBufferSRVRef IsoValuesSRV = GraphBuilder.CreateSRV(OutIsoValuesBuffer);
+
+			AddSphereGeneratorPass(GraphBuilder, Params, OutIsoValuesUAV);
+
+			FRHIGPUBufferReadback* isoReadback = new FRHIGPUBufferReadback(TEXT("PlanetGeneratorISO"));
+			FRHIGPUBufferReadback* typeReadback = new FRHIGPUBufferReadback(TEXT("PlanetGeneratorTYPE"));
+
+			AddEnqueueCopyPass(GraphBuilder, isoReadback, OutIsoValuesBuffer, 0u);
+			AddEnqueueCopyPass(GraphBuilder, typeReadback, OutTypeValuesBuffer, 0u);
+
+			auto RunnerFunc = [isoReadback, typeReadback, AsyncCallback, isoValueCount](auto&& RunnerFunc) ->
+				void {
+				if (isoReadback->IsReady() && typeReadback->IsReady()) {
+					FPlanetGeneratorOutput OutVal;
+
+					void* VBuf = isoReadback->Lock(0);
+					OutVal.outIsoValues.Append((float*)VBuf, isoValueCount);
+					isoReadback->Unlock();
+
+					void* VTypeBuf = typeReadback->Lock(0);
+					OutVal.outTypeValues.Append((uint32*)VTypeBuf, isoValueCount);
+					typeReadback->Unlock();
+
+					AsyncTask(ENamedThreads::GameThread, [AsyncCallback, OutVal]() {AsyncCallback(OutVal); });
+					delete isoReadback;
+				}
+				else {
+					AsyncTask(ENamedThreads::ActualRenderingThread, [RunnerFunc]() {
+						RunnerFunc(RunnerFunc); });
+				}
+				};
+			AsyncTask(ENamedThreads::ActualRenderingThread, [RunnerFunc]() {
+				RunnerFunc(RunnerFunc); });
+		}
+		else {} 
+
+	}
+	GraphBuilder.Execute();
+}
